93e7a215bb7d0a85772c69269ba8e7b30d9e373b
Frank Corso <frank@frankcorso.me>
2018-05-31 17:00:57 -0400

    Adds fields to load different repos

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
-// requires: vue.js
-
 var app = new Vue({
 	el: '#app',
 	data: {
+		user: 'fpcorso',
+		repo: 'quiz_master_next',
+		milestone: 50,
 		issues: [],
 		format: 'HTML'
 	},
@@ -13,12 +14,41 @@ var app = new Vue({
 			} else {
 				this.format = 'HTML';
 			}
+		},
+		loadIssues: function() {
+			fetch( 'https://api.github.com/repos/' + this.user + '/' + this.repo + '/issues?milestone=' + this.milestone + '&state=all' )
+				.then( ( response ) => {
+					return response.json();
+				})
+				.then( ( issueJson ) => {
+					let returnedIssues = [];
+					issueJson.forEach(issue => {
+						if ( ! issue.hasOwnProperty( 'pull_request' ) ) {
+							if ( 'closed' === issue.state ) {
+								let label = '';
+								if ( 0 < issue.labels.length ) {
+									label = issue.labels[0].name;
+								}
+								returnedIssues.push({
+									id: issue.number,
+									title: issue.title,
+									url: issue.url,
+									label: label
+								});
+							}
+						}
+					});
+					this.issues = returnedIssues;
+				})
 		}
+	},
+	mounted: function() {
+		this.loadIssues();
 	}
 });
 
 Vue.component( 'issue', {
 	props: ['issue', 'format'],
-	template: `<div class="issue"><span v-if="'markdown' == format">* Closed {{issue.label}}: {{issue.title}} ([Issue #{{issue.number}}]({{issue.url}}))</span>
-	<li v-else class="fixed"><span class="two">Closed {{issue.label}}</span> {{issue.title}} <a v-bind:href="issue.url" target="_blank">Issue #{{issue.number}}</a></li></div>`
+	template: `<div class="issue"><span v-if="'markdown' == format">* Closed {{issue.label}}: {{issue.title}} ([Issue #{{issue.id}}]({{issue.url}}))</span>
+	<li v-else class="fixed"><span class="two">Closed {{issue.label}}</span> {{issue.title}} <a v-bind:href="issue.url" target="_blank">Issue #{{issue.id}}</a></li></div>`
 });
