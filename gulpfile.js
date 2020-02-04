3e0faa2b3679e3823f78c72fc0d412f449871631
Frank Corso <frank@frankcorso.me>
2020-02-04 15:47:09 -0500

    Rewrites changelog generator to remove JS libraries

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,48 @@
 // Our main Gulp plugins
-var gulp       = require( 'gulp' );
-var htmlclean  = require( 'gulp-htmlclean' );
-var concat     = require( 'gulp-concat' );
-var deporder   = require( 'gulp-deporder' );
-var stripdebug = require( 'gulp-strip-debug' );
-var uglify     = require( 'gulp-uglify' );
-var minifyCSS  = require( 'gulp-csso' );
-var less       = require( 'gulp-less' );
-var del        = require( 'del' );
+const { src, dest, parallel, series } = require('gulp');
+const htmlclean  = require( 'gulp-htmlclean' );
+const concat     = require( 'gulp-concat' );
+const deporder   = require( 'gulp-deporder' );
+const stripdebug = require( 'gulp-strip-debug' );
+const uglify     = require( 'gulp-uglify' );
+const minifyCSS  = require( 'gulp-csso' );
+const del        = require( 'del' );
 
 // Relevant folders
-var folder = {
+const folder = {
 	src: 'src/',
 	build: 'build/'
-}
+};
 
 // HTML Task: Cleans HTML
-gulp.task( 'html', function() {
-	return gulp.src( folder.src + '*.html' )
+function html() {
+	return src( folder.src + '*.html' )
 		.pipe( htmlclean() )
-		.pipe( gulp.dest( folder.build ) )
-});
+		.pipe( dest( folder.build ) )
+}
 
 // JS Task: Concats all JS and uglifies
-gulp.task( 'js', function() {
-	return gulp.src( folder.src + 'js/*.js' )
+function js() {
+	return src( folder.src + 'js/*.js' )
 		.pipe( deporder() )
 		.pipe( concat( 'app.js' ) )
-		// .pipe( uglify() )
-		.pipe( gulp.dest( folder.build + 'js' ) )
-});
+		//.pipe( uglify() )
+		.pipe( dest( folder.build + 'js' ) )
+}
 
 // CSS Task: Concats all CSS and minifies
-gulp.task( 'css', function() {
-	return gulp.src( folder.src + 'less/*.less' )
-		.pipe( less() )
+function css() {
+	return src( folder.src + 'css/*.css' )
 		.pipe( minifyCSS() )
-		.pipe( gulp.dest( folder.build + 'css' ) )
-});
+		.pipe( dest( folder.build + 'css' ) )
+}
 
 // Deletes all build files
-gulp.task( 'reset', function() {
+function reset(cb) {
 	del( 'build/**/*' ).then( function( paths ) {
 		console.log( 'Deleted files and folders:\n', paths.join( '\n' ) );
 	});
-})
+	cb();
+}
 
-// Sets up watch task
-gulp.task( 'watch', function() {
-	gulp.watch( folder.src + '**/*', [ 'build' ] );
-});
-
-// Build the site
-gulp.task( 'build', [ 'html', 'css', 'js' ], function() {
-	return gulp.src( folder.src + 'assets/**/*' )
-		.pipe( gulp.dest( folder.build + 'assets' ) )
-});
-
-// Default task
-gulp.task( 'default', [ 'build', 'watch' ] );
+exports.default = series(reset, parallel(html, js, css));
